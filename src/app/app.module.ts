834b644f64a99cad47bf3fa860e014725f5c3670
ashutoshT1999 <atiwariiit@gmail.com>
2022-08-16 12:43:59 +0530

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PersonComponent } from './person/person.component';
 import { StudentComponent } from './student/student.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { FormsModule } from '@angular/forms';
+import {MatDialogModule} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonComponent,
-    StudentComponent
+    StudentComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,MatFormFieldModule,MatDatepickerModule
+    BrowserAnimationsModule, MatFormFieldModule, MatDatepickerModule, FormsModule,MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { person } from '../interface/interfaceperson.component';
+import { Form, NgForm } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '../dialog/dialog.component';
 @Component({
   selector: 'app-person',
   templateUrl: './person.component.html',
@@ -8,12 +11,32 @@ import { person } from '../interface/interfaceperson.component';
 export class PersonComponent implements OnInit {
   
   student:person[]=[
+    {fname:"ashu",mname:"",lname:"tiwari",dep:"btech",gender:"male",dob:"20/2/2000",address:"kanpur",pincode:"208012"},
+    {fname:"ashu",mname:"",lname:"tiwari",dep:"btech",gender:"male",dob:"20/2/2000",address:"kanpur",pincode:"208012"},
+    {fname:"ashu",mname:"",lname:"tiwari",dep:"btech",gender:"male",dob:"20/2/2000",address:"kanpur",pincode:"208012"},
+    {fname:"ashu",mname:"",lname:"tiwari",dep:"btech",gender:"male",dob:"20/2/2000",address:"kanpur",pincode:"208012"},
+    {fname:"ashu",mname:"",lname:"tiwari",dep:"btech",gender:"male",dob:"20/2/2000",address:"kanpur",pincode:"208012"},
+    {fname:"ashu",mname:"",lname:"tiwari",dep:"btech",gender:"male",dob:"20/2/2000",address:"kanpur",pincode:"208012"},
+    {fname:"ashu",mname:"",lname:"tiwari",dep:"btech",gender:"male",dob:"20/2/2000",address:"kanpur",pincode:"208012"},
+    {fname:"ashu",mname:"",lname:"tiwari",dep:"btech",gender:"male",dob:"20/2/2000",address:"kanpur",pincode:"208012"},
+    {fname:"ashu",mname:"",lname:"tiwari",dep:"btech",gender:"male",dob:"20/2/2000",address:"kanpur",pincode:"208012"},
     {fname:"ashu",mname:"",lname:"tiwari",dep:"btech",gender:"male",dob:"20/2/2000",address:"kanpur",pincode:"208012"}
+
   ];
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
    
- 
+  openDialog() {
+    this.dialog.open(DialogComponent);
+  }
   ngOnInit(): void {
+  }
+
+  forminfo(data:NgForm){
+    console.log(data.value);
+  }
+  popup()
+  {
+
   }
 
 }
